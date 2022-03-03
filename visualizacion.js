89842e64b54f2ba79afd55b9988d3a34a7c0346d
flavia <flavia.e.arroyo@gmail.com>
2022-03-03 19:51:26 -0300

    actualizacion porfolio

---
+++ visualizacion.js
@@ -0,0 +1,54 @@
+
+
+
+function certifjs(){
+    document.getElementById("certjs").style.display= "block"
+    document.getElementById("certhtmlcss").style.display= "none"
+    document.getElementById("certgitgithab").style.display= "none"
+    document.getElementById("certhabilidades").style.display= "none"
+}
+
+function certihtmlcss(){
+    document.getElementById("certhtmlcss").style.display= "block"
+    document.getElementById("certjs").style.display= "none"    
+    document.getElementById("certgitgithab").style.display= "none"
+    document.getElementById("certhabilidades").style.display= "none"
+
+}
+
+function certigit(){
+    document.getElementById("certgitgithab").style.display= "block"
+    document.getElementById("certhtmlcss").style.display= "none"
+    document.getElementById("certjs").style.display= "none"    
+    document.getElementById("certhabilidades").style.display= "none"
+
+}
+
+function certisoftskills(){
+    document.getElementById("certhabilidades").style.display= "block"
+    document.getElementById("certgitgithab").style.display= "none"
+    document.getElementById("certhtmlcss").style.display= "none"
+    document.getElementById("certjs").style.display= "none"    
+    
+
+}
+
+function diplomaargpro(){
+    document.getElementById("diplomaap").style.display="block"
+    document.getElementById("diplomarrhh").style.display="none"
+    document.getElementById("diplomacont").style.dysplay="none"
+}
+
+function diplomarrhh(){
+    document.getElementById("diplomarrhh").style.display="block"
+    document.getElementById("diplomaap").style.display="none"
+    document.getElementById("diplomacont").style.dysplay="none"
+}
+
+function diplomacont(){
+    document.getElementById("diplomacont").style.dysplay="block"
+    document.getElementById("diplomarrhh").style.display="none"
+    document.getElementById("diplomaap").style.display="none"
+
+}
+
